205e600f3348db3b03e7c4dd3aa330cbeca1edf9
MLopez <codermex.web@gmail.com>
2024-07-21 01:30:25 -0300

    mejorando rendimiento

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,31 +5,35 @@ document.addEventListener('DOMContentLoaded', () => {
     function setupPlayButton(playButtonId, audioId, waveId) {
         const playButton = document.getElementById(playButtonId);
         const audio = document.getElementById(audioId);
-        const selector = document.querySelectorAll('#' + waveId + ' li');
+        const selector = document.querySelectorAll(`#${waveId} li`);
         let isPlaying = false;
 
         playButton.addEventListener('click', () => {
-            let playMusic = localStorage.getItem('play_music');
-
             if (isPlaying) {
                 audio.pause();
                 playButton.innerHTML = '<i class="fas fa-play"></i>';
-                selector.forEach(li => li.classList.remove('ondas__animation'));
-                selector.forEach(li => li.classList.add('music__tamaño'));
+                toggleClasses(selector, 'ondas__animation', 'music__tamaño');
                 localStorage.removeItem('play_music');
             } else {
-                if (playMusic) {
+                if (localStorage.getItem('play_music')) {
                     return; // No reproducir si ya hay otro audio en reproducción
                 }
                 audio.play();
                 playButton.innerHTML = '<i class="fas fa-pause"></i>';
-                selector.forEach(li => li.classList.remove('music__tamaño'));
-                selector.forEach(li => li.classList.add('ondas__animation'));
+                toggleClasses(selector, 'music__tamaño', 'ondas__animation');
                 localStorage.setItem('play_music', audioId);
             }
             isPlaying = !isPlaying;
         });
     }
+
+    function toggleClasses(elements, classToRemove, classToAdd) {
+        elements.forEach(element => {
+            element.classList.remove(classToRemove);
+            element.classList.add(classToAdd);
+        });
+    }
+
     for (let i = 1; i <= 7; i++) {
         setupPlayButton(`playButton_${i}`, `music__audio-${i}`, `ondas_${i}`);
     }
